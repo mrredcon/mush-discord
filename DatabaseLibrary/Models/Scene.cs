aa9b30ea75844ddd57b97e002fe8e508eaef0cec
Connor McCormick <mrredcon@gmail.com>
2018-10-19 18:15:06 -0500

    Replace Active bool with enum

--- DatabaseLibrary/Models/Scene.cs
+++ DatabaseLibrary/Models/Scene.cs
@@ -10,11 +10,18 @@ namespace FalloutRPG.Models
         public string Title { get; set; }
         public string Description { get; set; }
 
-        public bool State { get; set; }
+        public SceneState State { get; set; }
 
         public string OwnerName { get; set; }
         public int OwnerId { get; set; }
 
         public virtual ICollection<Pose> Poses { get; set; }
+    }
+
+    public enum SceneState
+    {
+        Active,
+        Paused,
+        Finished
     }
 }
