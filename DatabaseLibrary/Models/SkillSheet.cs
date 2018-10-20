c81b07df818faa942c656787e48cb7eee2bc98e6
Connor McCormick <mrredcon@gmail.com>
2018-10-19 19:19:41 -0500

    Add new skills to SkillSheet

--- DatabaseLibrary/Models/SkillSheet.cs
+++ DatabaseLibrary/Models/SkillSheet.cs
@@ -2,19 +2,72 @@
 {
     public class SkillSheet : BaseModel
     {
-        public int CharacterId { get; set; }
-        public int Barter { get; set; }
-        public int EnergyWeapons { get; set; }
-        public int Explosives { get; set; }
-        public int Guns { get; set; }
-        public int Lockpick { get; set; }
-        public int Medicine { get; set; }
-        public int MeleeWeapons { get; set; }
-        public int Repair { get; set; }
-        public int Science { get; set; }
-        public int Sneak { get; set; }
-        public int Speech { get; set; }
-        public int Survival { get; set; }
+        // Strength
+        public int Archery { get; set; }
+        public int Athletics { get; set; }
+        public int Construction { get; set; }
+        public int HeavyWeapons { get; set; }
+        public int Intimidation { get; set; }
+        public int Melee { get; set; }
         public int Unarmed { get; set; }
+
+        // Perception
+        public int Alertness { get; set; }
+        public int Art { get; set; }
+        public int Brewing { get; set; }
+        public int Cooking { get; set; }
+        public int Deception { get; set; }
+        public int Explosives { get; set; }
+        public int FirstAid { get; set; }
+        public int Gunsmith { get; set; }
+        public int Husbandry { get; set; }
+        public int Investigation { get; set; }
+        public int Music { get; set; }
+        public int Repair { get; set; }
+        public int Security { get; set; }
+
+        // Endurance
+        public int Blacksmith { get; set; }
+        public int Resistance { get; set; }
+        public int Survival { get; set; }
+        public int Toughness { get; set; }
+
+        // Charisma
+        public int Barter { get; set; }
+        public int Command { get; set; }
+        public int Courage { get; set; }
+        public int Manipulation { get; set; }
+        public int Performance { get; set; }
+        public int Persuasion { get; set; }
+        public int Seduction { get; set; }
+        public int Streetwise { get; set; }
+
+        // Intelligence
+        public int EnergyWeapons { get; set; }
+        public int Hacking { get; set; }
+        public int History { get; set; }
+        public int Literature { get; set; }
+        public int Medicine { get; set; }
+        public int Pharmaceuticals { get; set; }
+        public int Science { get; set; }
+        public int Tactics { get; set; }
+        public int Technology { get; set; }
+
+        // Agility
+        public int Acrobatics { get; set; }
+        public int Dodge { get; set; }
+        public int Drive { get; set; }
+        public int Firearms { get; set; }
+        public int Pilot { get; set; }
+        public int Riding { get; set; }
+        public int Stealth { get; set; }
+        public int Tailoring { get; set; }
+
+        // Luck
+        public int Escape { get; set; }
+        public int Gamble { get; set; }
+        public int ImprovisedWeapons { get; set; }
+        public int JuryRig { get; set; }
+        public int Scavenge { get; set; }
     }
 }
