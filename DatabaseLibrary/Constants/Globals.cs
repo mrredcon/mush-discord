c6b9b5a2c7ffaf1381b57d0283b76d02d23a1ad6
Connor McCormick <mrredcon@gmail.com>
2018-10-19 19:14:27 -0500

    Hardcode new skills

--- DatabaseLibrary/Constants/Globals.cs
+++ DatabaseLibrary/Constants/Globals.cs
@@ -7,19 +7,20 @@ namespace FalloutRPG.Constants
     {
         public enum SkillType
         {
-            Barter,
-            EnergyWeapons,
-            Explosives,
-            Guns,
-            Lockpick,
-            Medicine,
-            MeleeWeapons,
-            Repair,
-            Science,
-            Sneak,
-            Speech,
-            Survival,
-            Unarmed
+            // Strength
+            Archery, Athletics, Construction, HeavyWeapons, Intimidation, Melee, Unarmed,
+            // Perception
+            Alertness, Art, Brewing, Cooking, Deception, Explosives, FirstAid, Gunsmith, Husbandry, Investigation, Music, Repair, Security,
+            // Endurance
+            Blacksmith, Resistance, Survival, Toughness,
+            // Charisma
+            Barter, Command, Courage, Manipulation, Performance, Persuasion, Seduction, Streetwise,
+            // Intelligence
+            EnergyWeapons, Hacking, History, Literature, Medicine, Pharmaceuticals, Science, Tactics, Technology,
+            // Agility
+            Acrobatics, Dodge, Drive, Firearms, Pilot, Riding, Stealth, Tailoring,
+            // Luck
+            Escape, Gamble, ImprovisedWeapons, JuryRig, Scavenge
         }
 
         public enum SpecialType
@@ -44,40 +45,175 @@ namespace FalloutRPG.Constants
 
         public static readonly Dictionary<string, SkillType> SKILL_ALIASES = new Dictionary<string, SkillType>(StringComparer.OrdinalIgnoreCase)
         {
-            { "Barter", SkillType.Barter },
+            // Strength
+            { "Archery", SkillType.Archery },
+            { "Archer", SkillType.Archery },
 
-            { "Energy", SkillType.EnergyWeapons },
-            { "Energy Weapons", SkillType.EnergyWeapons },
-            { "Energy Weapon", SkillType.EnergyWeapons },
-            { "EnergyWeapon", SkillType.EnergyWeapons },
-            { "EnergyWeapons", SkillType.EnergyWeapons },
+            { "Athletics", SkillType.Athletics },
+
+            { "Construction", SkillType.Construction },
+            { "Building", SkillType.Construction },
+
+            { "Heavy Weapons", SkillType.HeavyWeapons },
+            { "HeavyWeapons", SkillType.HeavyWeapons },
+
+            { "Intimidation", SkillType.Intimidation },
+
+            { "Melee", SkillType.Melee },
+            { "Melee Weapons", SkillType.Melee },
+
+            { "Unarmed", SkillType.Unarmed },
+            { "Hand to hand", SkillType.Unarmed },
+            { "Hand-to-hand", SkillType.Unarmed },
+            { "Fists", SkillType.Unarmed },
+
+            // Perception
+            { "Alertness", SkillType.Alertness },
+            { "Alert", SkillType.Alertness },
+
+            { "Art", SkillType.Art },
+ 
+            { "Brewing", SkillType.Brewing },
+            { "Brew", SkillType.Brewing },
+
+            { "Cooking", SkillType.Cooking },
+            { "Cook", SkillType.Cooking },
+
+            { "Deception", SkillType.Deception },
+            { "Decept", SkillType.Deception },
 
             { "Explosives", SkillType.Explosives },
 
-            { "Guns", SkillType.Guns },
+            { "First Aid", SkillType.FirstAid },
+            { "FirstAid", SkillType.FirstAid },
 
-            { "Lockpick", SkillType.Lockpick },
+            { "Gunsmith", SkillType.Gunsmith },
+            { "Gunmaking", SkillType.Gunsmith },
+            { "Gunbuilding", SkillType.Gunsmith },
 
-            { "Medicine", SkillType.Medicine },
-            { "Medic", SkillType.Medicine },
-            { "Doctor", SkillType.Medicine },
+            { "Husbandry", SkillType.Husbandry },
 
-            { "Melee", SkillType.MeleeWeapons },
-            { "MeleeWeapon", SkillType.MeleeWeapons },
-            { "MeleeWeapons", SkillType.MeleeWeapons },
-            { "Melee Weapons", SkillType.MeleeWeapons },
+            { "Investigation", SkillType.Investigation },
+            { "Investigate", SkillType.Investigation },
 
+            { "Music", SkillType.Music },
+ 
             { "Repair", SkillType.Repair },
+            { "Fix", SkillType.Repair },
 
-            { "Science", SkillType.Science },
+            { "Security", SkillType.Security },
+            { "Lockpick", SkillType.Security },
+            { "Lockpicking", SkillType.Security },
 
-            { "Sneak", SkillType.Sneak },
+            // Endurance
+            { "Blacksmith", SkillType.Blacksmith },
 
-            { "Speech", SkillType.Speech },
+            { "Resistance", SkillType.Resistance },
+            { "Resist", SkillType.Resistance },
 
             { "Survival", SkillType.Survival },
 
-            { "Unarmed", SkillType.Unarmed },
+            { "Toughness", SkillType.Toughness },
+            { "Tough", SkillType.Toughness },
+
+            // Charisma
+            { "Barter", SkillType.Barter },
+            { "Haggle", SkillType.Barter },
+            { "Trading", SkillType.Barter },
+            { "Trade", SkillType.Barter },
+
+            { "Command", SkillType.Command },
+
+            { "Courage", SkillType.Courage },
+
+            { "Manipulation", SkillType.Manipulation },
+            { "Manipulate", SkillType.Manipulation },
+
+            { "Performance", SkillType.Performance },
+
+            { "Persuasion", SkillType.Persuasion },
+            { "Persuade", SkillType.Persuasion },
+
+            { "Seduction", SkillType.Seduction },
+            { "Seduce", SkillType.Seduction },
+
+            { "Streetwise", SkillType.Streetwise },
+            { "Street smarts", SkillType.Streetwise },
+            { "Street smart", SkillType.Streetwise },
+
+            // Intelligence
+            { "EnergyWeapons", SkillType.EnergyWeapons },
+            { "EnergyWeapon", SkillType.EnergyWeapons },
+            { "Energy Weapons", SkillType.EnergyWeapons },
+            { "Energy Weapon", SkillType.EnergyWeapons },
+            { "Energy", SkillType.EnergyWeapons },
+
+            { "Hacking", SkillType.Hacking },
+            { "Hack", SkillType.Hacking },
+
+            { "History", SkillType.History },
+
+            { "Literature", SkillType.Literature },
+            { "Books", SkillType.Literature },
+            { "Book", SkillType.Literature },
+
+            { "Medicine", SkillType.Medicine },
+
+            { "Pharmaceuticals", SkillType.Pharmaceuticals },
+            { "Drugs", SkillType.Pharmaceuticals },
+            { "Drug", SkillType.Pharmaceuticals },
+
+            { "Science", SkillType.Science },
+
+            { "Tactics", SkillType.Tactics },
+
+            { "Technology", SkillType.Technology },
+            { "Tech", SkillType.Technology },
+
+            // Agility
+            { "Acrobatics", SkillType.Acrobatics },
+            { "Acrobat", SkillType.Acrobatics },
+
+            { "Dodge", SkillType.Dodge },
+ 
+            { "Drive", SkillType.Drive },
+            { "Driver", SkillType.Drive },
+            { "Driving", SkillType.Drive },
+
+            { "Firearms", SkillType.Firearms },
+            { "Firearm", SkillType.Firearms },
+            { "Guns", SkillType.Firearms },
+            { "Gun", SkillType.Firearms },
+
+            { "Pilot", SkillType.Pilot },
+            { "Piloting", SkillType.Pilot },
+
+            { "Riding", SkillType.Riding },
+
+            { "Stealth", SkillType.Stealth },
+
+            { "Tailoring", SkillType.Tailoring },
+
+            // Luck
+            { "Escape", SkillType.Escape },
+
+            { "Gamble", SkillType.Gamble },
+
+            { "ImprovisedWeapons", SkillType.ImprovisedWeapons },
+            { "ImprovisedWeapon", SkillType.ImprovisedWeapons },
+            { "Improvised Weapons", SkillType.ImprovisedWeapons },
+            { "Improvised Weapon", SkillType.ImprovisedWeapons },
+            { "Improvised Weaps", SkillType.ImprovisedWeapons },
+            { "Improvised Weps", SkillType.ImprovisedWeapons },
+            { "Improvised Weap", SkillType.ImprovisedWeapons },
+            { "Improvised Wep", SkillType.ImprovisedWeapons },
+            
+            { "JuryRig", SkillType.JuryRig },
+            { "Jury Rig", SkillType.JuryRig },
+            { "Jury Rigging", SkillType.JuryRig },
+
+            { "Scavenge", SkillType.Scavenge },
+            { "Scavenging", SkillType.Scavenge },
         };
 
         public static readonly Dictionary<string, SpecialType> SPECIAL_ALIASES = new Dictionary<string, SpecialType>(StringComparer.OrdinalIgnoreCase)
@@ -108,19 +244,20 @@ namespace FalloutRPG.Constants
 
         public static string[] SKILL_NAMES = new string[]
         {
-            "Barter",
-            "EnergyWeapons",
-            "Explosives",
-            "Guns",
-            "Lockpick",
-            "Medicine",
-            "MeleeWeapons",
-            "Repair",
-            "Science",
-            "Sneak",
-            "Speech",
-            "Survival",
-            "Unarmed"
+            // Strength
+            "Archery", "Athletics", "Construction", "HeavyWeapons", "Intimidation", "Melee", "Unarmed",
+            // Perception
+            "Alertness", "Art", "Brewing", "Cooking", "Deception", "Explosives", "FirstAid", "Gunsmith", "Husbandry", "Investigation", "Music", "Repair", "Security",
+            // Endurance
+            "Blacksmith", "Resistance", "Survival", "Toughness",
+            // Charisma
+            "Barter", "Command", "Courage", "Manipulation", "Performance", "Persuasion", "Seduction", "Streetwise",
+            // Intelligence
+            "EnergyWeapons", "Hacking", "History", "Literature", "Medicine", "Pharmaceuticals", "Science", "Tactics", "Technology",
+            // Agility
+            "Acrobatics", "Dodge", "Drive", "Firearms", "Pilot", "Riding", "Stealth", "Tailoring",
+            // Luck
+            "Escape", "Gamble", "ImprovisedWeapons", "JuryRig", "Scavenge"
         };
 
         public static string[] SPECIAL_NAMES = new string[]
